fa4159db4286774dcf7e1994a013f8d858f1208e
Bobby Lei <bobl53@gmail.com>
2021-05-04 11:25:05 +1200

    Bootstrap new mix app

---
+++ lib/bdaykata.ex
@@ -0,0 +1,18 @@
+defmodule Bdaykata do
+  @moduledoc """
+  Documentation for `Bdaykata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bdaykata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bdaykata_test.exs
@@ -0,0 +1,8 @@
+defmodule BdaykataTest do
+  use ExUnit.Case
+  doctest Bdaykata
+
+  test "greets the world" do
+    assert Bdaykata.hello() == :world
+  end
+end
