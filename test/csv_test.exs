3a0796ed895290be72de89274010e499449442f7
Bobby Lei <bobl53@gmail.com>
2021-05-05 09:55:19 +1200

    Add more example tests to highlight certain bugs

--- test/csv_test.exs
+++ test/csv_test.exs
@@ -16,6 +16,45 @@ defmodule CsvTest do
     assert [%{"" => ""}] = Csv.decode("\r\n\r\n")
   end
 
+
+  test "one record per line" do
+    assert [%{"aaa" => "zzz", "bbb" => "yyy", "ccc" => "xxx"}] ==
+    Csv.decode("aaa,bbb,ccc\r\nzzz,yyy,xxx\r\n")
+  end
+
+  test "optional trailing CRLF" do
+    assert [%{"aaa" => "zzz", "bbb" => "yyy", "ccc" => "xxx"}] ==
+    Csv.decode("aaa,bbb,ccc\r\nzzz,yyy,xxx")
+  end
+
+  test "double quotes" do
+    assert [%{"aaa" => "zzz", "bbb" => "yyy", "ccc" => "xxx"}] ==
+    Csv.decode("\"aaa\",\"bbb\",\"ccc\"\r\nzzz,yyy,xxx")
+  end
+
+  test "escape CRLF" do
+    assert [%{"aaa" => "zzz", "b\r\nbb" => "yyy", "ccc" => "xxx"}] ==
+    Csv.decode("\"aaa\",\"b\r\nbb\",\"ccc\"\r\nzzz,yyy,xxx")
+  end
+
+  test "double quote escaping" do
+    # Since we decided headers are mandatory, this test adds a line
+    # with empty values (CLRF,,) to the example from the RFC.
+    assert [%{"aaa" => "", "b\"bb" => "", "ccc" => ""}] ==
+      Csv.decode("\"aaa\",\"b\"\"bb\",\"ccc\"\r\n,,")
+  end
+
+  # this counterexample is taken literally from the RFC and # cannot work with the current implementation because maps # do not allow duplicate keys
+  test "dupe keys unsupported" do
+    csv =
+      "field_name,field_name,field_name\r\n" <>
+        "aaa,bbb,ccc\r\n" <> "zzz,yyy,xxx\r\n"
+
+    [map1, map2] = Csv.decode(csv)
+    assert ["field_name"] == Map.keys(map1)
+    assert ["field_name"] == Map.keys(map2)
+  end
+
   def csv_source() do
     let size <- pos_integer() do
       let keys <- header(size + 1) do
